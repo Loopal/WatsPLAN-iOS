85c0d8f4640dee680309f5ccbf4d96b1b0a1ca23
Wenjiu Wang <vanjor1014@gmail.com>
2020-06-13 11:59:35 -0700

    test

--- WatsPLAN/WatsPLAN/ContentView.swift
+++ WatsPLAN/WatsPLAN/ContentView.swift
@@ -10,7 +10,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment:.leading) {
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("Button")
+            }
+                .background(Color.black)
+            
+            Text("Welcome back, Warrior!")
+                .font(.title)
+            Text("Let's begin your degree check")
+                .foregroundColor(Color.gray)
+            
+            RoundedRectangle(cornerRadius: 10)
+                    .fill(Color.yellow)
+                    .frame(width: 350, height: 150.0)
+            
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("Load")
+                    .foregroundColor(Color.yellow)
+                    .frame(width: 200.0, height: 30.0)
+                    .background(Color.black)
+                    .cornerRadius(10)
+            }
+            .padding(.leading, 75)
+            .offset(y:-20)
+
+
+                
+        }
+        
+
+        
+
     }
 }
 
